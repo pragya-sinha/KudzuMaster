4fee65de3b32e9ba31c9cb18238126f16551b969
Gautam Bohara <Gautam_Bohara@BGMB124197-2.local>
2020-12-19 21:25:52 +0530

    code clean up

---
+++ src/App.js
@@ -0,0 +1,22 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {SafeAreaView, StyleSheet, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <>
+      <SafeAreaView>
+        <Text>Hello world</Text>
+      </SafeAreaView>
+    </>
+  );
+};
+
+export default App;
