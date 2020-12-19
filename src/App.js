28027b782ebdc73f665cd2758563bc3e743ead4d
Gautam Bohara <Gautam_Bohara@BGMB124197-2.local>
2020-12-19 21:54:13 +0530

    variables initial

--- src/App.js
+++ src/App.js
@@ -7,15 +7,25 @@
  */
 
 import React from 'react';
-import {SafeAreaView, StyleSheet, Text} from 'react-native';
+import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+
+
+//declaring global
+let test = 'test';
+let isAvaialble = true;
 
 const App = () => {
+
+  //local const cannot be modified
+  const testArray = [{name: 'test'}];
+
+  //local variable can be modified 
+  let name="test";
+
   return (
-    <>
-      <SafeAreaView>
-        <Text>Hello world</Text>
-      </SafeAreaView>
-    </>
+    <View>
+      <Text>{test}</Text>
+    </View>
   );
 };
 
